9b5d78f77ccd9edec30b00daed3d2782fbf5caa0
VladChV <vladchv@gmail.com>
2021-06-19 16:50:01 +0300

    ...
    ...

--- js/cookies.js
+++
@@ -1,7 +0,0 @@
-if (window.USER || document.cookie.replace(/(?:(?:^|.*;\s*)accepted\s*\=\s*([^;]*).*$)|^.*$/, "$1")==='1') {
-	document.getElementById('use-cookies').style.display='none';
-}
-$('.js-cookies-aacept').on('click', function () {
-	document.cookie='accepted=1;path=/';
-	$('#use-cookies').hide(true);
-});
